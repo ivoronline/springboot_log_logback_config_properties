58aa3aaac761060d164ebea6066476f6c87b070c
igoreta <ivoronline@gmail.com>
2021-03-17 19:06:24 +0100

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_log_logback_config_properties/SpringbootLogLogbackConfigPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_log_logback_config_properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLogLogbackConfigPropertiesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootLogLogbackConfigPropertiesApplication.class, args);
+  }
+
+}
